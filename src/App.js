60932f2a85a2d26359b26a826c3203475037f0ed
Pekikii <Vlad.Pecheanu@gmail.com>
2023-04-06 18:07:58 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -2,25 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    return (
+        <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-      TEST TEST
+        Edit <code>src/App.js</code> and save to reload.
+        TEST TEST
+        </p>
+        <p>
+        PULA MEA
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          Learn React
+        Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+        </div>
+    );
 }
 
 export default App;
