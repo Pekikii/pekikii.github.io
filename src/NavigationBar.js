faa4a2a3d58f6def08f881a04cc074c34f5b7d74
Pek <vlad.pecheanu@tum.de>
2024-10-10 14:09:24 +0200

    ignore build and add space to the left of navbar

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 function NavigationBar({ activeTab, handleSelect }) {
   return (
     <Navbar bg="light" variant="light" expand="lg">
-      <Navbar.Brand href="#home">Pek</Navbar.Brand>
+      <Navbar.Brand className="ms-3" href="#home">Pek</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav activeKey={activeTab} onSelect={handleSelect}>
